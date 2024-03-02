38acf05a38275c1c2a38032a7ed8a603e918317f
Val-d-emar <vhot@live.ru>
2024-03-02 11:04:17 +0400

    fix: return to working loop

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,49 @@
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+import { FastifyPluginAsyncTypebox, Type } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { GraphQLSchema, graphql } from 'graphql';
+
+
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
-  const {prisma} = fastify;
+  const { prisma } = fastify;
+
+//   const typeDefs = `
+//   type User {
+//     email: String!
+//     name: String
+//   }
+
+//   type Query {
+//     allUsers: [User!]!
+//   }
+// `;
+
+//   const resolvers = {
+//     Query: {
+//       allUsers: () => {
+//         return prisma.user.findMany();
+//       }
+//     }
+//   };
+//   const schema = new GraphQLSchema({
+//     query: new GraphQLObjectType({
+//       name: 'Query',
+//       fields: {
+//         hero: { type: characterInterface, ... },
+//       }
+//     }),
+//     ...
+//     // Since this schema references only the `Character` interface it's
+//     // necessary to explicitly list the types that implement it if
+//     // you want them to be included in the final schema.
+//     types: [humanType, droidType],
+//   })
+
+//   const Qschema = new GraphQLSchema({
+//     resolvers,
+//     typeDefs,
+//   });
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -14,9 +54,17 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return await prisma.user.findMany();
+      // return await prisma.user.findMany();
       // req.body.query
-      return {};
+      // req['prisma'] = prisma;
+      return await graphql({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        schema: Qschema,
+        source: req.body.query,
+        variableValues: req.body.variables,
+        contextValue: prisma,
+      });
+      // return {};
     },
   });
 };
